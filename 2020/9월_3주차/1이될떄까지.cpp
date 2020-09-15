fbbf70b41ffe142e6ce3a40773f8e8a87371e4d3
nayoung <kny8493@gmail.com>
2020-09-16 00:30:03 +0900

    [SEP_16th] 그리디 - 1이될때까지 - 숫자카드게임

---
+++ 2020/9월_3주차/1이될떄까지.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N, K, ans = 0, tmp=0;
+    cin >> N >> K;
+    while (N) {
+        tmp = N;
+
+        while (1) {
+            if (tmp % K != 0) break;
+            tmp = tmp / K;
+            ans++;
+            if (tmp == 1) {
+                cout << ans;
+                return 0;
+            }
+        }
+        N--;
+        ans++;
+
+    }
+    cout << ans;
+    return 0;
+
+}
---
+++ 2020/9월_3주차/숫자카드게임.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<algorithm>
+
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(0);
+	int N, M, ans = 0;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		int tmp = 1000;
+		for (int j = 0; j < M; j++) {
+			int card;
+			cin >> card;
+			if (tmp > card) {
+				tmp = card;
+			}
+		}
+		if (tmp > ans) {
+			ans = tmp;
+		}
+	}
+	cout << ans;
+	return 0;
+}
