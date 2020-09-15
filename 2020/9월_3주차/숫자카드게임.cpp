16ae9b096b0862c05a9191a928f5aaac52c72301
nayoung <kny8493@gmail.com>
2020-09-16 00:40:27 +0900

    Update 숫자카드게임.cpp
    최댓값 처리

--- 2020/9월_3주차/숫자카드게임.cpp
+++ 2020/9월_3주차/숫자카드게임.cpp
@@ -8,13 +8,14 @@ int main() {
 	int N, M, ans = 0;
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
-		int tmp = 1000;
+		int tmp = 1001;
 		for (int j = 0; j < M; j++) {
 			int card;
 			cin >> card;
 			if (tmp > card) {
 				tmp = card;
 			}
+		
 		}
 		if (tmp > ans) {
 			ans = tmp;
