a7d9763ae13625d2f30b2630407de417ca7c2431
nayoung <kny8493@gmail.com>
2020-09-18 23:40:34 +0900

    [SEP_18] 구현 - 상하좌우

---
+++ 2020/9월_3주차/상하좌우.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<cstdio>  
+#include<string>
+using namespace std;
+// 공간 벗어나면 무시
+// L -> left
+// R
+// U
+// D
+
+
+int N;
+string str;
+int main() {
+	cin >> N;
+	cin.ignore();
+	getline(cin, str);
+	int row = 1, col = 1;
+	
+	for (int i = 0; i < str.size(); i ++) {
+		char tmp = str[i];
+		switch (tmp){
+		case 'L':
+			if (col - 1 < 1)  break;
+			col --;
+			break;
+		case 'R' :
+			if (col + 1 > N) break;
+			col++;
+			break;
+		case 'U':
+			if (row - 1 < 1) break;
+			row--;
+			break;
+		case 'D':
+			if (row + 1 > N) break;
+			row++;
+			break;
+		}
+	}
+	cout << row << " "<<col;
+	return 0;
+
+}
