ddab3d05770e7056d170bf13cce2e852c8caf60a
nayoung <kny8493@gmail.com>
2020-09-19 02:26:25 +0900

    [SEP_18] 구현 - 게임개발 - 시각

---
+++ 2020/9월_3주차/시각.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <string>
+#include <iostream>
+#include<stdlib.h>
+using namespace std;
+
+bool check(int i, int j, int k) {
+	if (i % 10 == 3 || i / 10 == 3 || j / 10 == 3 || j % 10 == 3 || k / 10 == 3 || k % 10 == 3) { 
+		return true; 
+	}
+	return false;
+}
+
+int main() {
+	int n;
+	int ans=0;
+	cin >> n;
+
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j < 60; j++) {
+			for (int k = 0; k < 60; k++) {
+				
+				if (check(i, j, k))  ans++;
+			}
+		}
+	}
+	cout << ans;
+	return 0;
+}
