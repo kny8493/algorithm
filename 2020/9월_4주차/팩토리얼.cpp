8cb6ed1e6228d6dab1b1f251a33dc5191a9f4bc7
nayoung <kny8493@naver.com>
2020-09-26 19:08:53 +0900

    [SEP_26] 팩토리얼

---
+++ 2020/9월_4주차/팩토리얼.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int answer = 0;
+int factorial(int n) {
+	// n = 1 인경우 재귀함수 멈춤
+	if (n <= 1) {
+		return 1;
+	}
+	return n * factorial(n - 1);
+}
+
+int main (){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int num;
+	cin >> num;
+	
+	cout<<factorial(num);
+	
+}
