1ee2e7cc6740e9107e3856717d5454ad00891fe1
nayoung <kny8493@naver.com>
2020-09-30 23:20:10 +0900

    [SEP_30] 선택정렬알고리즘

---
+++ 2020/9월_5주차/선택정렬알고리즘.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <deque>
+#include <stack>
+#include <vector>
+#include<cstdio>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+int main (){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int num[] = { 7,5,9,0,3,1,6,2,4,8 };
+	// 선택 정렬
+	for (int i = 0; i <10; i++) {
+		int min = num[i];
+		int idx = i;
+		for (int j = i; j < 10; j++) {
+			if (num[j] < min) {
+				idx = j;
+				min = num[j];
+			}
+		}
+		swap(num[idx], num[i]);
+	}
+	for (int i = 0; i < 10; i++) {
+		cout << num[i] << " ";
+	}
+
+}
