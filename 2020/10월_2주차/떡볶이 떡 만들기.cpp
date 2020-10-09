07ad588d2c38f53257e66e0ae7f7bc3235b56581
nayoung <kny8493@naver.com>
2020-10-09 15:02:44 +0900

    [OCT_9] 떡볶이 떡 만들기

---
+++ 2020/10월_2주차/떡볶이 떡 만들기.cpp
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <deque>
+#include <stack>
+#include <vector>
+#include<cstdio>
+#include<algorithm>
+#include<queue>
+#include<map>
+#define MAX 100001
+using namespace std;
+int main (){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int cnt;
+	int etc;
+	vector<int> rice;
+	cin >> cnt >> etc;
+	for (int i = 0; i < cnt; i++) {
+		int tmp;
+		cin >> tmp;
+		rice.push_back(tmp);
+	}
+	int left, right;
+	sort(rice.begin(), rice.end());
+	left = 0;
+	right = rice.back();
+	int ans, mid;
+
+	while (left < right) {
+		ans = 0;
+		mid = (left + right) / 2;
+		for (int i = 0; i < cnt; i++) {
+			int tmp = rice[i] - mid < 0 ? 0 : rice[i] - mid;
+			ans += tmp;
+		}
+		if (ans == etc)  break;
+		else if (ans > etc) left++;
+		else right--;
+	}
+	cout << mid;
+
+}
