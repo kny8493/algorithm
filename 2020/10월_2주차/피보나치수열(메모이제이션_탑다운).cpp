cfd3086f4a32725e868a43fba5bb6172c554cc4d
nayoung <kny8493@naver.com>
2020-10-11 01:03:52 +0900

    [OCT_10] 피보나치 수열(메모리제이션 _ 탑다운 방식)

---
+++ 2020/10월_2주차/피보나치수열(메모이제이션_탑다운).cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <deque>
+#include <stack>
+#include <vector>
+#include<cstdio>
+#include<algorithm>
+#include<queue>
+#include<map>
+#define MAX 100001
+using namespace std;
+
+int num[6];
+
+int fibo(int x) {
+    if (x == 1 || x == 0) {
+        return 1;
+    }
+    // 이미 계산이 된거면
+    if (num[x] != 0) {
+        return num[x];
+    }
+    num[x] = fibo(x - 1) + fibo(x - 2);
+    return num[x];
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout<<fibo(5);
+
+}
