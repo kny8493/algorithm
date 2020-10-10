8ce64b15b0132ba39db702732c41fe1771691d4f
nayoung <kny8493@naver.com>
2020-10-11 00:47:21 +0900

    [OCT_10] 프로그래머스 데모 테스트

---
+++ 2020/10월_2주차/프로그래머스_데모테스트(직사각형).cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <deque>
+#include <stack>
+#include <vector>
+#include<cstdio>
+#include<algorithm>
+#include<queue>
+#include<map>
+#define MAX 100001
+using namespace std;
+
+vector<int> solution(vector<vector<int> > v) {
+	vector<int> ans;
+	int x, y;
+
+	if (v[0][0] == v[1][0]) {
+		y = v[0][1] + v[1][1] - v[2][1];
+		x = v[2][0];
+	}
+	else if (v[0][1] == v[1][1]) {
+		x = v[0][0] + v[1][0] - v[2][0];
+		y = v[2][1];
+	}
+	else {
+		x = v[0][0] + v[1][0] - v[2][0];
+		y = v[0][1] + v[1][1] - v[2][1];
+	}
+	ans.push_back(x);
+	ans.push_back(y);
+
+	return ans;
+}
+int main (){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	vector<vector<int> > v={ {1,4}, {3,4},{3,10} };
+	solution(v);
+
+}
