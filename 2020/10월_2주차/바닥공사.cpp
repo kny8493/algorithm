00606b729ee7a10593defeab07bc7c714251e2e7
nayoung <kny8493@naver.com>
2020-10-11 22:01:38 +0900

    [OCT_11] 바닥공사- DP

---
+++ 2020/10월_2주차/바닥공사.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <deque>
+#include <stack>
+#include <vector>
+#include<cstdio>
+#include<algorithm>
+#include<queue>
+#include<map>
+#define MAX 10001
+
+#define MOX 796796
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    int num[MAX];
+    cin >> N;
+    num[1] = 1;
+    num[2] = 3;
+    for(int i = 3; i <= N; i++) {
+        num[i] = num[i - 1] + (num[i - 2] * 2);
+    }
+    cout << num[N];
+}
