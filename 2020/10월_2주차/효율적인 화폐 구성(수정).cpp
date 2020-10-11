f3bf4a686326ba65b78eb7b53cb228fc07b8e21a
nayoung <kny8493@naver.com>
2020-10-11 22:51:51 +0900

    [OCT_11] 효율적인 화폐 구성 - 수정 -  다이나믹 프로그래밍(Dynamic Programming) 진행(보텀업)

---
+++ 2020/10월_2주차/효율적인 화폐 구성(수정).cpp
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <deque>
+#include <stack>
+#include <vector>
+#include<cstdio>
+#include<algorithm>
+#include<queue>
+#include<map>
+#define MAX 10001
+
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int num[MAX];
+    int N, M;
+    int ans = 0;
+    vector<int> v;
+
+    cin >> N >> M;
+    for (int i = 1; i <= M; i++) {
+        num[i] = 10001;
+    }
+    num[0] = 0;
+    for (int i = 0; i < N; i++) {
+        int tmp;
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = v[i]; j <= M; j++) {
+            if (num[j - v[i]] != MAX) {
+                num[j] = min(num[j], num[j - v[i]] + 1);
+            }
+        }
+    }
+    int answer = num[M] == MAX ? -1 : num[M];
+    cout << answer;
+}
