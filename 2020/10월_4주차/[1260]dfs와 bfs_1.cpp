52d417b3621112eec360241f44fefb19695ce6a1
nayoung <kny8493@naver.com>
2020-10-24 01:07:06 +0900

    [OCT_24] 1260- dfs와 bfs - 가장 약한 유형(그래프)이므로 반복이 필요

---
+++ 2020/10월_4주차/[1260]dfs와 bfs_1.cpp
@@ -0,0 +1,60 @@
+﻿#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <deque>
+#include <stack>
+#include <vector>
+#include<cstdio>
+#include<algorithm>
+#include<memory.h>
+#include<queue>
+#include<map>
+#include <sstream>
+
+#define MAX 10
+
+using namespace std;
+
+bool visit[MAX];
+int N, M, V;
+vector<int> v[MAX];
+void bfs(int i) {
+	cout << "\n";
+	visit[i] = true;
+	queue<int> q;
+	q.push(i);
+	while (!q.empty()) {
+		int tmp = q.front();
+		q.pop();
+		cout << tmp << " ";
+		for (int k = 0; k < v[tmp].size(); k++) {
+			if (!visit[v[tmp][k]]) {
+				q.push(v[tmp][k]);
+				visit[v[tmp][k]] = true;
+			}
+		}
+	}
+}
+void dfs(int i) {
+	visit[i] = true;
+	cout << i << " ";
+	for (int k = 0; k < v[i].size(); k++) {
+		if (!visit[v[i][k]]) {
+			dfs(v[i][k]);
+		}
+	}
+}
+int main() {
+	cin >> N >> M >> V;
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		cin >> a >> b;
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+	dfs(V);
+
+	memset(visit, false, sizeof(visit));
+	bfs(V);
+}
